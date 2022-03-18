2d4bb64efddf0c34d20dbbaac752cc5110722424
Arseny Ivanov <arsivanov@google.com>
2022-03-18 01:46:52 -0300

    Add more tests

--- sample_test.py
+++ sample_test.py
@@ -1,18 +1,54 @@
 from sample import to_roman
 import pytest
 
-def test_smallest_valid():
-    assert to_roman(1) == "I", "1 should translate to I"
+
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        (1, "I"),
+        (5, "V"),
+        (10, "X"),
+        (50, "L"),
+        (100, "C"),
+        (500, "D"),
+        (1000, "M"),
+    ]
+)
+def test_smallest_valid(input, output):
+    assert to_roman(input) == output
+
 
 def test_largest_valid():
     assert to_roman(3999) == "MMMCMXCIX", "3999 should translate to 3999"
 
-def test_invalid():
+
+def test_invalid_too_many_IXC():
+    for v in ['IIII', 'XXXX', 'CCCC']:
+        with pytest.raises(ValueError):
+            to_roman(v)
+
+
+def test_invalid_too_many_VLD():
+    for v in ['VV', 'LL', 'DD']:
+        with pytest.raises(ValueError):
+            to_roman(v)
+
+
+def test_invalid_not_a_num():
+    with pytest.raises(ValueError):
+        to_roman("string")
+
+
+def test_invalid_zero():
     with pytest.raises(ValueError):
         to_roman(0)
 
+
+def test_invalid_negative():
     with pytest.raises(ValueError):
         to_roman(-100)
 
+
+def test_invalid_too_big():
     with pytest.raises(ValueError):
         to_roman(4000)
